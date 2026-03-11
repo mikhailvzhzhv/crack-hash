ac4038577b92176ebfa52d34fd748480380ad4da
mikhailvzhzhv <mihaelvozhzhov@gmail.com>
2026-03-12 00:25:35 +0700

    Добавил скорость перебора

--- shared/models/result.go
+++ shared/models/result.go
@@ -3,8 +3,9 @@ package models
 import "github.com/google/uuid"
 
 type Result struct {
-	RequestID  uuid.UUID `json:"request_id"`
-	PartNumber int       `json:"part_number"`
-	PartCount  int       `json:"part_count"`
-	Words      []string  `json:"words"`
+	RequestID        uuid.UUID `json:"request_id"`
+	PartNumber       int       `json:"part_number"`
+	PartCount        int       `json:"part_count"`
+	Words            []string  `json:"words"`
+	AvgExecutionTime int       `json:"avg_execution_time"`
 }
